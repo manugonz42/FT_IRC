2a82bee153e16fde941e56957ca3c85657a63d03
jrubio-m <jrubio-m@student.42madrid.com>
2025-10-18 19:22:36 +0200

    deleted all

--- src/utils/strToUpper.cpp
+++
@@ -1,9 +0,0 @@
-#include "Ircserv.hpp"
-
-std::string strToUpper(const std::string& str) {
-    std::string result = str;
-    for (size_t i = 0; i < result.length(); ++i) {
-        result[i] = std::toupper(result[i]);
-    }
-    return result;
-}
